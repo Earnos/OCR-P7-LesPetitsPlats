90fcfb75c7360b6273ccf3bcacded1bbe537a2ab
Earnos <lcyril33@gmail.com>
2023-10-16 11:20:34 +0200

    test tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui'],
+        'serif': ['ui-serif', 'Georgia'],
+        'mono': ['ui-monospace', 'SFMono-Regular'],
+        'body': ['"Open Sans"'],
+      },
+      textColor: {
+        colors: {
+          'yellow': '#FFD15B',
+        },
+      },
+      extend: {},
+    },
+    plugins: [],
+}
