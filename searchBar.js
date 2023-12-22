b6c50668498b51be266754ca94a60650ce5bdea0
Earnos <lcyril33@gmail.com>
2023-12-22 16:42:26 +0100

    filter searchBar

--- searchBar.js
+++ searchBar.js
@@ -1,12 +1,35 @@
-// Search Bars methode 2 (with filter())
+// ancienne logique for loop remplacer par un filter() methode
+const searchInput = document.getElementById('searchInput')
 
-// import getData from './data.js'
-// // import getRecipes from './cards.js'
+searchInput.addEventListener('keyup', (e) => {
+    const cards = document.querySelectorAll('.cards')
+    const characters = e.target.value
+    filterRecipes(characters, cards)
+})
 
-// // let recipes = getData()
+function filterRecipes(letters, elements) {
+    if (letters.length > 2) {
+        const filteredElements = Array.from(elements).filter((element) =>
+            element.textContent.toLowerCase().includes(letters)
+        )
 
-//version a test
-// Sort methode with filter()
+        elements.forEach((element) => {
+            if (filteredElements.includes(element)) {
+                element.style.display = 'block'
+            } else {
+                element.style.display = 'none'
+            }
+        })
+    }
+
+    if (letters.length < 2) {
+        elements.forEach((element) => {
+            element.style.display = 'block'
+        })
+    }
+}
+
+//Sort methode with filter()
 // function filterRecipes() {
 //     const searchInput = document.getElementById('searchInput')
 //     const searchTerm = searchInput.value.toLowerCase()
@@ -24,18 +47,17 @@
 // function displayRecipes(recipesToShow) {
 //     const recipeList = document.querySelector('.main-section')
 
-// Clean actual recipes list
-// recipeList.innerHTML = ''
+//     //Clean actual recipes list
+//     recipeList.innerHTML = ''
 
-// add filtered recipes
-//getRecipes()
-// recipesToShow.forEach((recipe) => {
-//     const listItem = document.createElement('li')
-//     listItem.textContent = recipe
-//     recipeList.appendChild(listItem)
-// })
-//}
-
+//     //add filtered recipes
+//     getRecipes()
+//     recipesToShow.forEach((recipe) => {
+//         const listItem = document.createElement('li')
+//         listItem.textContent = recipe
+//         recipeList.appendChild(listItem)
+//     })
+// }
 //Afficher toutes les recettes au chargement initial
 // window.onload = function () {
 //     displayRecipes(recipes)
