90636a08c53a9621b73181722d6dfa7c741661d8
Earnos <lcyril33@gmail.com>
2023-12-22 16:13:21 +0100

    new methode

--- searchBar.js
+++ searchBar.js
@@ -1,28 +1,3 @@
-// Sort method with FOR loop
-const searchInput = document.getElementById('searchInput')
-
-searchInput.addEventListener('keyup', (e) => {
-    const cards = document.querySelectorAll('.cards')
-    const characters = e.target.value
-    filterRecipes(characters, cards)
-})
-
-function filterRecipes(letters, elements) {
-    if (letters.length > 2) {
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].textContent.toLowerCase().includes(letters)) {
-                elements[i].style.display = 'block'
-            } else {
-                elements[i].style.display = 'none'
-            }
-        }
-    }
-    if (letters.length < 2) {
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].style.display = 'block'
-        }
-    }
-}
 // Search Bars methode 2 (with filter())
 
 // import getData from './data.js'
