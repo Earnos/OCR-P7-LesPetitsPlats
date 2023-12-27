0d08cd91243a8d6728b05468b9d39e7c4e50d925
Earnos <lcyril33@gmail.com>
2023-12-27 10:36:39 +0100

    filter conditions

--- searchBar.js
+++ searchBar.js
@@ -1,4 +1,4 @@
-// ancienne logique for loop remplacer par un filter() methode
+// filter recipes with searchBar
 const searchInput = document.getElementById('searchInput')
 
 searchInput.addEventListener('keyup', (e) => {
@@ -28,37 +28,3 @@ function filterRecipes(letters, elements) {
         })
     }
 }
-
-//Sort methode with filter()
-// function filterRecipes() {
-//     const searchInput = document.getElementById('searchInput')
-//     const searchTerm = searchInput.value.toLowerCase()
-
-//     // Filtrer min 3 characters
-//     const filteredRecipes = recipes.filter(
-//         (recipe) =>
-//             recipe.toLowerCase().includes(searchTerm) && searchTerm.length >= 3
-//     )
-
-//     // display filtered recipes
-//     displayRecipes(filteredRecipes)
-// }
-
-// function displayRecipes(recipesToShow) {
-//     const recipeList = document.querySelector('.main-section')
-
-//     //Clean actual recipes list
-//     recipeList.innerHTML = ''
-
-//     //add filtered recipes
-//     getRecipes()
-//     recipesToShow.forEach((recipe) => {
-//         const listItem = document.createElement('li')
-//         listItem.textContent = recipe
-//         recipeList.appendChild(listItem)
-//     })
-// }
-//Afficher toutes les recettes au chargement initial
-// window.onload = function () {
-//     displayRecipes(recipes)
-// }
